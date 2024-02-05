d03682e9b6d4c40b4eb91a2f8914f2025c6e48dc
sarveshsta <sarvesh@seizetheads.com>
2024-02-05 15:03:39 +0530

    fdbskfjd

--- algo_trade_form/settings.py
+++ algo_trade_form/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'algo_trade_form_app',
-    'corsheaders'
+    'corsheaders',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
